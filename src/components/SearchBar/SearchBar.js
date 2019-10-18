20f6f61232548e428d836cdc9115a37e5cf393f2
IsleBeeGun <IsleBeeGun@gmail.com>
2019-10-18 22:25:23 +0300

    Add method renderSortByOptions()

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './SearchBar.css';
+
+let sortByOptions = {
+    'Nearest': "xxxxxx",
+    'Highest Rated': 'xxxxxxx',
+    'Most Reviewed': 'xxxxxx'
+};
+
+class SearchBar extends React.Component {
+    renderSortByOptions() {
+        
+    }
+}
