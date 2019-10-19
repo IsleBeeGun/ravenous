84490b9a9703baf946d7689777e910ff5f15035c
IsleBeeGun <IsleBeeGun@gmail.com>
2019-10-19 13:08:16 +0300

    Remove logo and complete App.js return

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import logo from '../../logo.svg';
 import './App.css';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
 
 function App() {
   return (
-    
+    <div className="App">
+      <h1>ravenous</h1>
+      <SearchBar />
+      <BusinessList />
+    </div>
   );
 }
 
