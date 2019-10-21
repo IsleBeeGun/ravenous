205a5e3e7ccfa12478a6ec1ab66dee1893e0f2d8
IsleBeeGun <IsleBeeGun@gmail.com>
2019-10-21 14:26:08 +0300

    BusinessList is now acting via array

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -15,13 +15,15 @@ let business = {
   reviewCount: 90
 };
 
+const businesses = [business, business, business, business, business, business];
+
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar />
-        <BusinessList />
+        <BusinessList businesses={businesses}/>
       </div>
     );
   }
