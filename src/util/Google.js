36a274b3014c8b46519b230a80678ed153aaf297
IsleBeeGun <IsleBeeGun@gmail.com>
2019-10-21 22:02:29 +0300

    Stop work till google IMG problem is set

---
+++ src/util/Google.js
@@ -0,0 +1,26 @@
+const apiKey = "AIzaSyC455u9Z-koM7HLLXX8GNkg82DXE3clo7w";
+const google = {
+    searchGoogle(query) {
+        return fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${query}&locationbias=ipbias&language=en&type=restaurant&maxprice=2&key=${apiKey}`)
+        .then(response => {
+            return response.json();
+        })
+        .then(jsonResponse => {
+            return jsonResponse.results.map(business => {
+                console.log(business); /* !!!!!!  */
+                return {
+                    id: business.id,                        /* CHECK */
+                    imageSrc: business.photos,  
+                    name: business.name,                     /* CHECK */
+                    adress: business.formatted_address,     /* CHECK */
+                    city: business.location.city,
+                    state: business.location.state,
+                    zipCode: business.location.zipCode,
+                    category: business.types[0],               /* CHECK */
+                    rating: business.rating,                    /* CHECK */
+                    reviewCount: business.user_ratings_total   /* CHECK */
+                }
+            });
+        });
+    }
+};  
