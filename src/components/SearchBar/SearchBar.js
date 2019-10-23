6100e4b711b6661d03cbba660e23ccb4270cf616
IsleBeeGun <islebeegun@gmail.com>
2019-10-23 20:09:41 +0300

    Change in SearchBar -> (keyword, type, sortBy)

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -11,23 +11,23 @@ class SearchBar extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            term: '',
-            location: '',
+            keyword: '',
+            type: '',
             sortBy: 'nearest'
         };
-        this.handleTermChange = this.handleTermChange.bind(this);
-        this.handleLocationChange = this.handleLocationChange.bind(this);
+        this.handleKeywordChange = this.handleKeywordChange.bind(this);
+        this.handleTypeChange = this.handleTypeChange.bind(this);
         this.handleSearch = this.handleSearch.bind(this);
     }
     handleSearch(event) {
-        this.props.searchGoogle(this.state.term, this.state.location, this.state.sortBy);
+        this.props.searchGoogle(this.state.keyword, this.state.type, this.state.sortBy);
         event.preventDefault();
     }
-    handleTermChange(event) {
-        this.setState({ term: event.target.value });
+    handleKeywordChange(event) {
+        this.setState({ keyword: event.target.value });
     }
-    handleLocationChange(event) {
-        this.setState({ location: event.target.value});
+    handleTypeChange(event) {
+        this.setState({ type: event.target.value});
     }
     getSortByClass(sortByOption) {
          if (sortByOption === this.state.sortBy) {
@@ -53,8 +53,8 @@ class SearchBar extends React.Component {
                     </ul>
                 </div>
                 <div className="SearchBar-fields">
-                    <input placeholder="Search Businesses" onChange={this.handleTermChange} />
-                    <input placeholder="Where?" onChange={this.handleLocationChange} />
+                    <input placeholder="Search Businesses" onChange={this.handleKeywordChange} />
+                    <input placeholder="Where?" onChange={this.handleTypeChange} />
                 </div>
                 <div className="SearchBar-submit">
                     <a onClick={this.handleSearch}>Let's Go</a>
